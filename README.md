f520889dc32931e423801ea244766fcefa93139a
MarcLazo <lazolatorre@et.esiea.fr>
2018-07-08 20:24:25 +0200

    Last modif

--- README.md
+++ README.md
@@ -1,11 +1,32 @@
 # CallWeatherDesk
 
-1.  Create a Dialogflow agent
-1.  Go to your agent's settings and [Restore from zip](https://dialogflow.com/docs/agents#export_and_import) using the `weather-agent.zip` in this directory (Note: this will overwrite your existing agent)
+## Setup Instructions
+
+### Setup: WWO Weather API
+
+1.  Get a WWO API key, by going to https://developer.worldweatheronline.com/api/ and following the instructions to get an API key that includes forecasts 14 days into the future
+1.  Paste your API key for the value of the apiKey on line 1 of functions/src/WEATHER_API_KEY.js
+
+### Steps
+
+1.  Create a Dialogflow agent : Select Default Language "French - fr" and Default Time Zone : "(GMT+1:00)"
+1.  Go to your agent's settings and [Restore from zip](https://dialogflow.com/docs/agents#export_and_import) using the `CallWeatherDesk.zip` in this directory (Note: this will overwrite your existing agent)
 1.  `cd` to the `functions` directory
 1.  Run `npm install`
 1.  Install the Firebase CLI by running `npm install -g firebase-tools`
 1.  Login to your Google account with `firebase login`
 1.  Add your project to the sample with `firebase use [project ID]` [find your project ID here](https://dialogflow.com/docs/agents#settings)
-1.  Run `firebase deploy --only functions:dialogflowFulfillmentLibAdvancedSample`
-1.  Paste the URL into your Dialogflow agent's fulfillment
+1.  Run `npm run deploy` and take note of the endpoint where the fulfillment webhook has been published. It should look like `Function URL (yourAction): https://${REGION}-${PROJECT}.cloudfunctions.net/yourAction` or `Project Console: https://console.firebase.google.com/project/${PROJECT}/overview`, In the second case :
+    1.  Go to [this address](https://console.firebase.google.com/project/${PROJECT}/functions/list)
+    1.  The function address is under the word `Request` and should look like `https://${REGION}-${PROJECT}.cloudfunctions.net/dialogflowFirebaseFulfillment`
+1.  Go to the Dialogflow console and select _Fulfillment_ from the left navigation menu.
+1.  Enable _Webhook_, set the value of _URL_ to the `Function URL` from the previous step, then click _Save_.
+1.  Select _Intents_ from the left navigation menu. Select the `weather` intent, scroll down to the end of the page and click _Fulfillment_, check _Use webhook_ and then click _Save_. This will allow you to have the welcome intent be a basic webhook intent to test.
+    1.  Do the same with all the other intent.
+1.  Run `quel temps fait-il à Paris` for example in the Google right form
+1.  If google answer `Une erreur est survenue lors de la récupération des infos météo.` that mean you have to [upgrade your Google Formule Firebase](https://console.firebase.google.com/pricing/) to `Flame Plan`. The `Spark Plan` doesn't allow Outbound networking
+
+## Run Tests
+
+1.  `cd` to the `functions` directory
+1.  Run `npm run test`
--- functions/src/index.js
+++ functions/src/index.js
@@ -1,50 +1,68 @@
 // See https://github.com/dialogflow/dialogflow-fulfillment-nodejs
 // for Dialogflow fulfillment library docs, samples, and to report issues
-import * as functions from 'firebase-functions';
-import { WebhookClient } from 'dialogflow-fulfillment';
-import convertDateToApiFormat from './utils';
-import WeatherQuestionInfo from './WeatherQuestionInfo';
-import { weatherApiRequest, WetherApiSchema } from './WeatherApi';
-import weatherResponse from './WeatherResponse';
+import * as functions from "firebase-functions";
+import { WebhookClient } from "dialogflow-fulfillment";
+import convertDateToApiFormat from "./utils";
+import WeatherQuestionInfo from "./WeatherQuestionInfo";
+import { weatherApiRequest, WetherApiSchema } from "./WeatherApi";
+import weatherResponse from "./WeatherResponse";
+import { wetherApiResponseType } from "./flow-typed/callWeatherDesk";
 
-process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
+process.env.DEBUG = "dialogflow:debug"; // enables lib debugging statements
 
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest(
   (request: {}, response: {}): void => {
-    const webhookClient: WebhookClient = new WebhookClient({ request, response });
+    const webhookClient: WebhookClient = new WebhookClient({
+      request,
+      response
+    });
     const { body }: {} = request;
+
+    console.log("body");
+    console.log(body);
 
     async function reponseTheAnswer(agent: {}) {
       // stock user request's infos in an objetc
-      const weatherQuestion: WeatherQuestionInfo = new WeatherQuestionInfo(body);
+      const weatherQuestion: WeatherQuestionInfo = new WeatherQuestionInfo(
+        body
+      );
 
       // Check if the user asked a specific city.
       if (!weatherQuestion.isAddressKnown()) {
-        agent.add('Dans quelle ville voullez-vous que je recherche cette information ?');
-        return;
-      }
-
-      // get the wether inforation
-      let reponseApi: WetherApiSchema;
-      try {
-        reponseApi = await weatherApiRequest(
-          weatherQuestion.address,
-          convertDateToApiFormat(weatherQuestion.date),
+        agent.add(
+          "Dans quelle ville voullez-vous que je recherche cette information ?"
         );
-      } catch (err) {
-        console.log(`${err}`);
+      } else {
+        try {
+          // get the wether inforation
+          const reponseApi: wetherApiResponseType = await weatherApiRequest(
+            weatherQuestion.address,
+            convertDateToApiFormat(weatherQuestion.date)
+          );
+          // convert the Api response in an Object
+          const weatherInfo: WeatherQuestionInfo = new WetherApiSchema(
+            reponseApi
+          );
+
+          // personalized the answer
+          const finalResponseString: string = weatherResponse(
+            weatherInfo,
+            weatherQuestion
+          );
+
+          // add the weather desctiption to the response
+          agent.add(finalResponseString);
+        } catch (err) {
+          console.log(`${err}`);
+          // tell the user an error appened
+          agent.add(
+            "Une erreur est survenue lors de la récupération des infos météo."
+          );
+        }
       }
-
-      // convert the Api response in an Object
-      const weatherInfo: WeatherQuestionInfo = new WeatherQuestionInfo(reponseApi);
-
-      // personalized the answer
-      const finalResponseString: string = weatherResponse(weatherInfo, weatherQuestion);
-
-      agent.add(finalResponseString);
     }
 
     // Intent's declaration functions
     webhookClient.handleRequest(reponseTheAnswer);
-  },
+  }
 );
